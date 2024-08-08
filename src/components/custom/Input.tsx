7b5b014fbd361e449768085cb588267955c683e2
Abdulazeez Olalere <olalereazeez11@gmail.com>
2024-08-09 00:27:43 +0100

    FEAT: implement input field

--- src/components/custom/Input.tsx
+++ src/components/custom/Input.tsx
@@ -1,18 +1,49 @@
-import React from "react";
+"use client";
+import React, { useRef, useState } from "react";
 
 type Props = React.InputHTMLAttributes<HTMLInputElement>;
 
-export const Input: React.FC<Props> = (props: Props) => {
+export const Input: React.FC<Props> = ({ className, ...props }: Props) => {
+  const [value, setValue] = useState<string>("");
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+  const handleContainerClick = () => {
+    containerRef.current?.querySelector("input")?.focus();
+  };
+
   return (
-    <div className="relative w-full flex flex-col justify-center">
-      <input
-        {...props}
-        placeholder=" "
-        className={`h-14 px-4 border-none rounded-md bg-black-on-neutral text-13px leading-5 focus:pt-3 focus:outline-[1.5px] focus:outline-primary-border focus:ring-4 focus:ring-primary-ring w-full text-13px peer ${props.className}`}
-      />
-      <label className="absolute left-4 top-1/2  transform -translate-y-1/2 text-gray-500 transition-all duration-200 ease-in-out pointer-events-none peer-placeholder-shown:text-13px peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-1/2 peer-focus:top-5 peer-focus:text-[0.625rem] peer-focus:text-primary-border peer-valid:top-5 peer-valid:text-[0.625rem] peer-valid:text-primary-border">
-        {props.placeholder}
-      </label>
+    <div
+      ref={containerRef}
+      onClick={handleContainerClick}
+      className="relative w-full"
+    >
+      <div
+        className={`h-14 px-4 py-1  rounded-md bg-black-on-neutral text-13px flex items-center transition-all duration-200 ease-in-out border-[1.5px] border-transparent focus-within:border-primary-border focus-within:ring-4 focus-within:ring-primary-ring ${className}`}
+      >
+        <input
+          {...props}
+          className={`w-full bg-transparent border-none text-black-900 focus:outline-none text-13px leading-5 peer focus:translate-y-[0.5rem] ${
+            value ? "translate-y-[0.5rem]" : ""
+          }`}
+          placeholder=" "
+          onChange={handleChange}
+          value={value}
+        />
+        <label
+          className={`absolute top-1/2 text-black-subdued  translate-y-[-50%] left-4 transform transition-all duration-200 ease-in-out pointer-events-none
+            ${
+              value || props.value
+                ? "top-[1.15rem] text-[0.625rem]"
+                : "peer-focus:top-[1.15rem] peer:translate-y-0 text-13px"
+            } 
+            peer-focus:top-[1.15rem] peer-focus:text-[0.625rem] `}
+        >
+          {props.placeholder}
+        </label>
+      </div>
     </div>
   );
 };
--- src/components/landing/contact-us/FormComponent.tsx
+++ src/components/landing/contact-us/FormComponent.tsx
@@ -6,11 +6,17 @@ const FormComponent = () => {
       <div className="flex flex-col gap-y-4">
         <div className="flex gap-x-4 flex-grow">
           <div className="flex-1">
-            <Input placeholder="First name" />
+            <Input type="text" placeholder="First name" />
           </div>
           <div className="flex-1">
-            <Input />
+            <Input type="text" placeholder="Last name" />
           </div>
+        </div>
+        <div>
+          <Input type="email" placeholder="Email" />
+        </div>
+        <div>
+          <Input type="text" placeholder="Website URL" />
         </div>
       </div>
     </div>
