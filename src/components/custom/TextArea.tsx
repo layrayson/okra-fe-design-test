1532e68c206af90bd625930a92203ad07325456b
Abdulazeez Olalere <olalereazeez11@gmail.com>
2024-08-09 08:19:55 +0100

    FEAT: correct text-araa outline

--- src/components/custom/TextArea.tsx
+++ src/components/custom/TextArea.tsx
@@ -1,12 +1,17 @@
 type Props = React.TextareaHTMLAttributes<HTMLTextAreaElement> & {
   className?: string;
+  outlineClassName?: string;
 };
 
 export const TextArea = (props: Props) => {
   return (
-    <textarea
-      {...props}
-      className={`p-3 rounded-md w-full bg-black-on-neutral text-13px text-black-900 placeholder:text-black-subdued font-medium outline-[1.5px] outline-transparent focus:outline-primary-border focus:ring-4 focus:ring-primary-ring h-40 ${props.className}`}
-    />
+    <div
+      className={`p-1 rounded-md bg-black-on-neutral text-13px flex items-center transition-all duration-200 ease-in-out border-[1.5px] border-transparent focus-within:border-primary-border focus-within:ring-4 focus-within:ring-primary-ring h-40 ${props.outlineClassName}`}
+    >
+      <textarea
+        {...props}
+        className={`p-2 w-full text-13px text-black-900 placeholder:text-black-subdued font-medium h-full bg-transparent border-none focus:outline-none ${props.className}`}
+      />
+    </div>
   );
 };
